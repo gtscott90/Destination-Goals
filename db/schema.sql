10db813556e3bd1805e56543f358058ea4091230
Greg Scott <gtscott90@gmail.com>
2021-02-20 13:54:44 -0500

    gs/updated schema and seeds files

---
+++ db/schema.sql
@@ -0,0 +1,23 @@
+DROP DATABASE IF EXISTS goals_DB;
+CREATE database goals_DB;
+
+USE goals_DB;
+
+CREATE TABLE bigGoal (
+  id INT AUTO_INCREMENT NOT NULL,
+  goal_name VARCHAR(30) NOT NULL,
+  PRIMARY KEY (id)
+);
+
+CREATE TABLE milestones (
+  id INT AUTO_INCREMENT NOT NULL,
+  milestone_num INT,
+  datetime_created TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  current_datetime TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  title VARCHAR(100),
+  goal_id INT NOT NULL,
+  PRIMARY KEY (id)
+);
+
+SELECT * FROM bigGoal;
+SELECT * FROM milestones; 
---
+++ db/seeds.sql
@@ -0,0 +1,46 @@
+-- INSERT INTO tableName (column 1, column 2) VALUES (value for column 1, value for column 2)
+
+INSERT INTO bigGoal (id, goal_name) VALUES (1, "Play Piano");
+INSERT INTO bigGoal (id, goal_name) VALUES (2, "Learn Spanish");
+INSERT INTO bigGoal (id, goal_name) VALUES (3, "Bake Homemade Bread");
+INSERT INTO bigGoal (id, goal_name) VALUES (4, "Learn to Make a Web Page");
+INSERT INTO bigGoal (id, goal_name) VALUES (5, "Learn to Sing");
+
+INSERT INTO milestones (id, milestone_num, title, goal_id) VALUES (1, 1, "Understand the Clefs and how they correspond to the Keys on the Piano",  1);
+INSERT INTO milestones (id, milestone_num, title, goal_id) VALUES (2, 2, "Learn how to play the Major and Minor Scales",  1);
+INSERT INTO milestones (id, milestone_num, title, goal_id) VALUES (3, 3, "Understand and learn how to play Chords",  1);
+INSERT INTO milestones (id, milestone_num, title, goal_id) VALUES (4, 4, "Learn rhythm, note values, and meter",  1);
+INSERT INTO milestones (id, milestone_num, title, goal_id) VALUES (5, 5, "Start playing songs",  1);
+
+INSERT INTO milestones (id, milestone_num, title, goal_id) VALUES (6, 1, "Studying vocabulary for greetings and daily routine",  2);
+INSERT INTO milestones (id, milestone_num, title, goal_id) VALUES (7, 2, "Listening to music and watching movies in Spanish",  2);
+INSERT INTO milestones (id, milestone_num, title, goal_id) VALUES (8, 3, "Studying vocab for people/places/relations",  2);
+INSERT INTO milestones (id, milestone_num, title, goal_id) VALUES (9, 4, "Studying vocab for verbs/objects/things",  2);
+INSERT INTO milestones (id, milestone_num, title, goal_id) VALUES (10, 5, "Writing journal entries in Spanish",  2);
+INSERT INTO milestones (id, milestone_num, title, goal_id) VALUES (11, 6, "Reading text in Spanish",  2);
+INSERT INTO milestones (id, milestone_num, title, goal_id) VALUES (12, 7, "Conversations with native speakers",  2);
+
+INSERT INTO milestones (id, milestone_num, title, goal_id) VALUES (13, 1, "Scaling and mixing",  3);
+INSERT INTO milestones (id, milestone_num, title, goal_id) VALUES (14, 2, "Kneading",  3);
+INSERT INTO milestones (id, milestone_num, title, goal_id) VALUES (15, 3, "Fermentation",  3);
+INSERT INTO milestones (id, milestone_num, title, goal_id) VALUES (16, 4, "Shaping and Proofing",  3);
+INSERT INTO milestones (id, milestone_num, title, goal_id) VALUES (17, 5, "Baking",  3);
+
+INSERT INTO milestones (id, milestone_num, title, goal_id) VALUES (18, 1, "Introduction to the Web",  4);
+INSERT INTO milestones (id, milestone_num, title, goal_id) VALUES (19, 2, "Intro to HTML",  4);
+INSERT INTO milestones (id, milestone_num, title, goal_id) VALUES (20, 3, "Advanced HTML",  4);
+INSERT INTO milestones (id, milestone_num, title, goal_id) VALUES (21, 4, "Intro to CSS",  4);
+INSERT INTO milestones (id, milestone_num, title, goal_id) VALUES (22, 5, "CSS Layout",  4);
+INSERT INTO milestones (id, milestone_num, title, goal_id) VALUES (23, 6, "Intro to Javascript",  4);
+INSERT INTO milestones (id, milestone_num, title, goal_id) VALUES (24, 7, "Variables and Functions",  4);
+INSERT INTO milestones (id, milestone_num, title, goal_id) VALUES (25, 8, "Objects and the DOM",  4);
+INSERT INTO milestones (id, milestone_num, title, goal_id) VALUES (26, 9, "Uploading to GitHub",  4);
+INSERT INTO milestones (id, milestone_num, title, goal_id) VALUES (27, 10, "Publishing your page",  4);
+
+INSERT INTO milestones (id, milestone_num, title, goal_id) VALUES (28, 1, "Understand the structure of the voice and the body ",  5);
+INSERT INTO milestones (id, milestone_num, title, goal_id) VALUES (29, 2, "How to breathe and support your voice",  5);
+INSERT INTO milestones (id, milestone_num, title, goal_id) VALUES (30, 3, "How to sing the basic vowels A, E, I, O, U",  5);
+INSERT INTO milestones (id, milestone_num, title, goal_id) VALUES (31, 4, "Introduce scales switching vowels",  5);
+INSERT INTO milestones (id, milestone_num, title, goal_id) VALUES (32, 5, "Basic scales using words",  5);
+INSERT INTO milestones (id, milestone_num, title, goal_id) VALUES (33, 6, "Advanced Scales using words",  5);
+INSERT INTO milestones (id, milestone_num, title, goal_id) VALUES (34, 7, "Practice with a simple song",  5);
