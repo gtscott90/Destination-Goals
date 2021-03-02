0e0a21c085f9ce570658f2b7ae5d20e2645cebb3
Joel Straley <joelstraley@gmail.com>
2021-03-02 18:31:07 -0500

    fixed mysql connection

--- server.js
+++ server.js
@@ -32,17 +32,17 @@ initializePassport(
  */
 const PORT = process.env.PORT || 8080;
 
-// if (process.env.JAWSDB_URL){
-//   connection = mysql.createConnection(process.env.JAWSDB_URL);
-// } else {
-// connection = mysql.createConnection({
-//   host: 'localhost',
-//   port: 3306,
-//   user: 'root',
-//   password: process.env.PASSWORD,
-//   database: "goals_DB"
-// });
-// };
+if (process.env.JAWSDB_URL){
+ connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+connection = mysql.createConnection({
+ host: 'localhost',
+ port: 3306,
+   user: 'root',  
+   password: process.env.PASSWORD,
+  database: "goals_DB"
+ });
+ };
 
 
 
